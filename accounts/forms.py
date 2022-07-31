f9df7b9f5438ac6b66f123745b0ec8356a4969ca
Azubuine Samuel <azubuinesamuel@gmail.com>
2022-07-31 16:24:55 +0100

    Implemented User Registratio

---
+++ accounts/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from accounts.models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Enter Your Password"
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Confirm Password"
+    }))
+
+    class Meta:
+        model = Account
+        fields = ["first_name", 'last_name',
+                  "email", "password", "phone_number"]
+        labels = {
+            "username": " Your Name",
+            "first_name": "Your First Name",
+            "last_name": "Your Last name",
+            "email": "Your Email Address",
+            "phone_number": "Your Phone Number"
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields["first_name"].widget.attrs["placeholder"] = "Enter Your First Name"
+        self.fields["last_name"].widget.attrs["placeholder"] = "Enter Your Last Name"
+        self.fields["phone_number"].widget.attrs["placeholder"] = "Enter Your Phone Number"
+        self.fields["password"].widget.attrs["placeholder"] = "Enter Your Password"
+        self.fields["confirm_password"].widget.attrs["placeholder"] = "Confirm Your Password"
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-control"
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(" Password doesn't match!")
+
+
+class LoginForm(forms.ModelForm):
+    pass
+
+
+class LogoutForm(forms.ModelForm):
+    pass
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
+from accounts.forms import RegistrationForm
+from accounts.models import Account
+
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            password = form.cleaned_data["password"]
+            phone_number = form.cleaned_data["phone_number"]
+            confirm_password = form.cleaned_data["confirm_password"]
+            email = form.cleaned_data["email"]
+            username = email.split('@')[0]
+
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, username=username,
+                                               email=email, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/register.html", context)
+
+
+def login(request):
+    return render(request, "accounts/login.html")
+
+
+def logout(request):
+    return
