a2ece2dcb5a300e4c29f7fd89009464e6591bc15
Azubuine Samuel <azubuinesamuel@gmail.com>
2022-08-01 11:07:05 +0100

    Implemented Verify Email

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import redirect, render
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
+# verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 from .forms import RegistrationForm, LoginForm
 from .models import Account
 
@@ -24,8 +32,25 @@ def register(request):
                                                email=email, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, "Registration Successful")
-            return redirect('register')
+
+            # send Verification
+            current_site = get_current_site(request)
+            mail_subject = " Please Activate your Account"
+            message = render_to_string("accounts/account_verification_email.html", {
+                "user": user,
+                "domain": current_site,
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "token": default_token_generator.make_token(user),
+
+            })
+
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+
+            send_email.send()
+
+            # messages.success(request, "Thank You for registering with us. We have a sent you a mail []!")
+            return redirect('/accounts/login/?command=verification&email=' + email)
     else:
         form = RegistrationForm()
         context = {
@@ -43,8 +68,8 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
         if user is not None:
             auth.login(request, user)
-            messages.success(request, "Welcome")
-            return redirect('home')
+            messages.success(request, "You're Logged In!")
+            return redirect('dashboard')
 
         # user = Account.objects.get(email=email)
         # if user.password == password:
@@ -62,3 +87,27 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You are logged out")
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        user_pk = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=user_pk)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None:
+        user.is_active = True
+        user.save()
+        messages.success(
+            request, "Congratulations! Your account is activated.")
+        return redirect('login')
+
+    else:
+        messages.error(request, "Invalid Activation Link")
+        return redirect('register')
+
+
+# @login_required(login_url='login')
+def dashboard(request):
+    return render(request, "accounts/dashboard.html")
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,6 +143,13 @@ MESSAGE_TAGS = {
     50: 'critical'
 }
 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'azubuinesamuel@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
